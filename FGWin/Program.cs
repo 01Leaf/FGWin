65c3e4e7a4d22cb6e5c547aafa4d7476aed9aeb7
Uchuu Tamashi <ytc@mit.edu>
2014-08-26 17:34:07 +0800

    improved detection
    Implemented new detection method to ensure detection of freeform
    applications like osu!

--- FGWin/Program.cs
+++ FGWin/Program.cs
@@ -9,33 +9,44 @@ namespace FGWin
 {
     class Program
     {
-        /// <summary>The GetForegroundWindow function returns a handle to the foreground window.</summary>
-        [DllImport("user32.dll")]
-        private static extern IntPtr GetForegroundWindow();
+        
 
 
         static void Main(string[] args)
         {
-            string Current = "";
+            List<string> Current = new List<string>();
+            Process[] processCollection = Process.GetProcesses();
+            foreach (Process prc in processCollection)
+            {
+                Current.Add(prc.ProcessName);
+            }
 
             while (true)
             {
-                Process[] processCollection = Process.GetProcesses();
+                processCollection = Process.GetProcesses();
                 foreach (Process prc in processCollection)
                 {
-                    if (prc.MainWindowHandle == GetForegroundWindow() && prc.ProcessName!="" &&prc.ProcessName!=Current)
+                    if (prc.ProcessName!="" && !Current.Contains(prc.ProcessName))
                     {
-                        
+                        try { Console.WriteLine("PRCPath=\"" + prc.MainModule.FileName + "\""); }
+                        catch { }
                         Console.WriteLine("PRCName=\""+prc.ProcessName+"\"");
-                        Console.WriteLine("$SYS_READY?BROADCAST(" + prc.ProcessName + " activated)");
+                        Console.WriteLine("BROADCAST(PRCName+\" is activated\")");
                         if (prc.MainWindowTitle != "")
                         {
                             Console.WriteLine("PRCTitle=\"" + prc.MainWindowTitle + "\"");
                         }
-
-                        Current = prc.ProcessName;
+                        
                     }
                 }
+
+                Current.Clear();
+
+                foreach (Process prc in processCollection)
+                {
+                    Current.Add(prc.ProcessName);
+                }
+
                 System.Threading.Thread.Sleep(500);
             }
         }
