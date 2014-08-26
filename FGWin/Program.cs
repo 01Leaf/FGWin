c53e6b9b04fac287bd6cbe50e54a0703dc9f3b61
Uchuu Tamashi <ytc@mit.edu>
2014-08-26 14:07:26 +0800

    init

---
+++ FGWin/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace FGWin
+{
+    class Program
+    {
+        /// <summary>The GetForegroundWindow function returns a handle to the foreground window.</summary>
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+
+        static void Main(string[] args)
+        {
+            string Current = "";
+
+            while (true)
+            {
+                Process[] processCollection = Process.GetProcesses();
+                foreach (Process prc in processCollection)
+                {
+                    if (prc.MainWindowHandle == GetForegroundWindow() && prc.ProcessName!="" &&prc.ProcessName!=Current)
+                    {
+                        
+                        Console.WriteLine("PRCName=\""+prc.ProcessName+"\"");
+                        Console.WriteLine("$SYS_READY?BROADCAST(" + prc.ProcessName + " activated)");
+                        if (prc.MainWindowTitle != "")
+                        {
+                            Console.WriteLine("PRCTitle=\"" + prc.MainWindowTitle + "\"");
+                        }
+
+                        Current = prc.ProcessName;
+                    }
+                }
+                System.Threading.Thread.Sleep(500);
+            }
+        }
+    }
+}
